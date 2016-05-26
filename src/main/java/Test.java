6ab793bdb94be8f172fa9c34b08d161c24c60011
walidTrabelsi <trabelsi@gmx.at>
2016-05-26 22:02:05 +0200

    new version

--- src/main/java/Test.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by walidtrabelsi on 17/02/16.
- */
-public class Test {
-}
--- src/main/webapp/js/app/app.js
+++
@@ -1,6 +0,0 @@
-
-define(function(require){
-   var mainCtrl = require('./controllers/mainCtrl');
-   var aboutCtrl = require('./controllers/aboutCtrl');
-   var config = require('config');
-});
